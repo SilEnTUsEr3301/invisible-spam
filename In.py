7137f523251fe89889db24f19747f896eab734bd
SilEnTUsEr3301 <76544906+SilEnTUsEr3301@users.noreply.github.com>
2021-10-09 22:07:48 -0500

    Add files via upload

---
+++ In.py
@@ -0,0 +1,39 @@
+import amino
+import time
+import requests
+
+client=amino.Client()
+email=input("Email: ")
+password=input("Password: ")
+client.login(email=email,password=password)
+
+print("logged in")
+
+n=input("Community/chat link: ")
+fok=client.get_from_code(n)
+id=client.get_from_code(n).objectId
+cid=fok.path[1:fok.path.index("/")]
+print(cid)
+subclient=amino.SubClient(comId=cid,profile=client.profile)
+
+print("Joined community")
+
+@client.event("on_text_message")
+def on_text_message(data):
+	while True:
+			subclient.send_message(chatId=data.message.chatId,message=" .",messageType=109)
+			print(f"Text spam started")
+
+print("Spam Bot started")			
+def socketRoot():
+	j=0
+	while True:
+		if j>=300:
+			print("Updating socket.......")
+			client.close()
+			client.start()
+			print("Socket updated")
+			j=0
+		j=j+1
+		time.sleep(1)
+socketRoot()			
